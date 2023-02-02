b3f2d3e0a35326ae5586be34eabea06b0762c339
Winfort02 <winfort02@gmail.com>
2023-02-02 21:55:10 +0800

    Add try catch to category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -22,16 +22,26 @@ class CategoryController extends Controller
      */
     public function index(Request $request)
     {
-        $keywords = $request->keywords;
-        $category = Category::where(function ($query) use ($keywords) {
-            if ($keywords) {
-                $query->where('category_name', 'like', '%' . $keywords . '%');
-            }
-        })
-            ->orderBy('id', 'DESC')
-            ->paginate();
+        try {
+            
+            $keywords = $request->keywords;
+            $category = Category::where(function ($query) use ($keywords) {
+                if ($keywords) {
+                    $query->where('category_name', 'like', '%' . $keywords . '%');
+                }
+            })
+                ->orderBy('id', 'DESC')
+                ->paginate();
 
-        return CategoryResource::collection($category);
+            return CategoryResource::collection($category);
+
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -41,8 +51,18 @@ class CategoryController extends Controller
      */
     public function show_all_category()
     {
-        $category = Category::where('is_active', true)->get();
-        return CategoryResource::collection($category);
+        try {
+
+            $category = Category::where('is_active', true)->get();
+            return CategoryResource::collection($category);
+
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 
@@ -54,9 +74,18 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        $category = Category::findOrFail($id);
+        try {
+            
+            $category = Category::findOrFail($id);
+            return new CategoryResource($category);
 
-        return new CategoryResource($category);
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 
@@ -68,19 +97,29 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //create Category
-        $user = Auth::user();
-        $data = $request->only('category_name', 'description', 'is_active');
-        $category = Category::create($data);
-        UserLog::create([
-            'user_id' => $user->id,
-            'logs' => 'Category Management',
-            'remarks' => 'Added new category ' . $request->category_name,
-            'date' => Carbon::now()->format('Y-m-d'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        return new CategoryResource($category);
+        try {
+            
+            //create Category
+            $user = Auth::user();
+            $data = $request->only('category_name', 'description', 'is_active');
+            $category = Category::create($data);
+            UserLog::create([
+                'user_id' => $user->id,
+                'logs' => 'Category Management',
+                'remarks' => 'Added new category ' . $request->category_name,
+                'date' => Carbon::now()->format('Y-m-d'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+            return new CategoryResource($category);
+
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -92,23 +131,33 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $user = Auth::user();
-        $category = Category::find($id);
-        $category->category_name = $request->category_name;
-        $category->description = $request->description;
-        $category->is_active = $request->is_active;
-        $category->save();
+        try {
+            
+            $user = Auth::user();
+            $category = Category::find($id);
+            $category->category_name = $request->category_name;
+            $category->description = $request->description;
+            $category->is_active = $request->is_active;
+            $category->save();
 
-        UserLog::create([
-            'user_id' => $user->id,
-            'logs' => 'Category Management',
-            'remarks' => 'Update category detail',
-            'date' => Carbon::now()->format('Y-m-d'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+            UserLog::create([
+                'user_id' => $user->id,
+                'logs' => 'Category Management',
+                'remarks' => 'Update category detail',
+                'date' => Carbon::now()->format('Y-m-d'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
 
-        return new CategoryResource($category->refresh());
+            return new CategoryResource($category->refresh());
+
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
@@ -119,21 +168,31 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        $user = Auth::user();
-        $category = Category::findOrFail($id);
-        $cat_name = $category->category_name;
-        $category->delete();
+        try {
+            
+            $user = Auth::user();
+            $category = Category::findOrFail($id);
+            $cat_name = $category->category_name;
+            $category->delete();
 
-        UserLog::create([
-            'user_id' => $user->id,
-            'logs' => 'Category Management',
-            'remarks' => 'Delete category '. $cat_name,
-            'date' => Carbon::now()->format('Y-m-d'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+            UserLog::create([
+                'user_id' => $user->id,
+                'logs' => 'Category Management',
+                'remarks' => 'Delete category '. $cat_name,
+                'date' => Carbon::now()->format('Y-m-d'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
 
-        return response()->noContent();
+            return response()->noContent();
+            
+        } catch (Exception $e) {
+            
+            if($e->getCode() == 0) {
+                return response()->json(['message' => 'BRAND NOT FOUND'], Response::HTTP_NOT_FOUND);
+            } else
+                return response()->json(['message' => 'SERVER ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 
